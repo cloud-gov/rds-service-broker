50fea75b0b23ecdc3b9e5339ec0158b7a87159a4
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-22 22:19:39 -0400

    reformat spec hash

--- spec/create_db_spec.rb
+++ spec/create_db_spec.rb
@@ -7,21 +7,26 @@ describe 'create_db' do
     ENV['VPC_GROUP_ID'] = 'bar'
 
     expect_any_instance_of(Aws::RDS::Client).to receive(:create_db_instance).with(
-      :engine=>"postgres",
-      :db_name=>"c2",
-      :db_instance_identifier=>"cf-c2-staging",
-      :master_username=>"c2",
-      :master_user_password=>"randompass",
-      :tags=>[{:key=>"client", :value=>"CAP-20140930-20150930-01"}],
-      :auto_minor_version_upgrade=>true,
-      :backup_retention_period=>7,
-      :db_subnet_group_name=>"foo",
-      :publicly_accessible=>false,
-      :vpc_security_group_ids=>["bar"],
-      :allocated_storage=>5,
-      :db_instance_class=>"db.m1.small",
-      :multi_az=>false,
-      :storage_type=>"standard"
+      engine: 'postgres',
+      db_name: 'c2',
+      db_instance_identifier: 'cf-c2-staging',
+      master_username: 'c2',
+      master_user_password: 'randompass',
+      tags: [
+        {
+          key: 'client',
+          value: 'CAP-20140930-20150930-01'
+        }
+      ],
+      auto_minor_version_upgrade: true,
+      backup_retention_period: 7,
+      db_subnet_group_name: 'foo',
+      publicly_accessible: false,
+      vpc_security_group_ids: ['bar'],
+      allocated_storage: 5,
+      db_instance_class: 'db.m1.small',
+      multi_az: false,
+      storage_type: 'standard'
     )
     expect_any_instance_of(Aws::RDS::Client).to receive(:wait_until)
 
