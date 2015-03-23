80323d8b3d42bb3e39753805c853c837d7e24ea5
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-22 23:07:38 -0400

    add spec for resulting database URL

--- spec/db_creator_spec.rb
+++ spec/db_creator_spec.rb
@@ -1,10 +1,12 @@
 describe 'DbCreator' do
   describe '.run' do
-    it "sends the creation info to the AWS API" do
+    before do
       # TODO unset after test
       ENV['SUBNET'] = 'foo'
       ENV['VPC_GROUP_ID'] = 'bar'
+    end
 
+    it "sends the creation info to the AWS API" do
       expect_any_instance_of(Aws::RDS::Client).to receive(:create_db_instance).with(
         engine: 'postgres',
         db_name: 'c2',
@@ -35,6 +37,20 @@ describe 'DbCreator' do
       expect_any_instance_of(Aws::RDS::Client).to receive(:describe_db_instances).and_return(instances_resp)
 
       RdsServiceBroker::DbCreator.run
+    end
+
+    it "prints the database URL" do
+      expect_any_instance_of(Aws::RDS::Client).to receive(:create_db_instance)
+      expect_any_instance_of(Aws::RDS::Client).to receive(:wait_until)
+
+      endpoint = double(address: 'foo.bar.com', port: 5432)
+      instance = instance_double(Aws::RDS::DBInstance, endpoint: endpoint)
+      instances_resp = double(db_instances: [instance])
+      expect_any_instance_of(Aws::RDS::Client).to receive(:describe_db_instances).and_return(instances_resp)
+
+      expect {
+        RdsServiceBroker::DbCreator.run
+      }.to output(%r{postgresql://c2:randompass@foo\.bar\.com:5432/c2}).to_stdout
     end
   end
 end
